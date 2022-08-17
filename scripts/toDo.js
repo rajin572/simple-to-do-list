05179a62bda2db21de040e6e1491d476e3a1a0c1
Din Islam Rajin <dirrazin572@gmail.com>
2022-08-18 01:45:28 +0600

    first commit

---
+++ scripts/toDo.js
@@ -0,0 +1,20 @@
+// like as a to do list
+document.getElementById('upadated-input').addEventListener('click', function add(){
+    const inputFild = document.getElementById('input-fild');
+    const inputValue = inputFild.value;
+    const inputUl = document.getElementById('input-ul');
+    const newList = document.createElement('li');
+    const newListAtr = document.createAttribute('class');
+    newListAtr.value = 'list text-start m-1 p-2 border-bottom border-3 customli';
+    newList.setAttributeNode(newListAtr)
+    const len = inputUl.childNodes.length+")"+" ";
+    const finalLen = len;
+    if(inputValue === "" || inputValue === undefined || inputValue === null || inputValue === NaN){
+        console.log('plese input something');
+    }
+    else{
+        newList.innerText =finalLen + inputValue;
+        inputUl.appendChild(newList)
+    }
+    inputFild.value = '';
+})
